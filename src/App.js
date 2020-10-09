af9bf325f04a336f8100e344a10f0595d4bab3b6
jstonedev <jstonedev521@gmail.com>
2020-10-09 15:41:56 -0400

    update: create Home, About, Products, Login & Cart pages. setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
+import LoginPage from "./pages/LoginPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello, World!</h1>
-		</div>
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<HomePage />
+				</Route>
+				<Route exact path="/about">
+					<AboutPage />
+				</Route>
+				<Route exact path="/products">
+					<ProductsPage />
+				</Route>
+				<Route exact path="/login">
+					<LoginPage />
+				</Route>
+				<Route exact path="/cart">
+					<CartPage />
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const HomePage = (props) => {
+	return (
+		<div>
+			<h1>Home Page</h1>
+		</div>
+	);
+};
+
+HomePage.propTypes = {};
+
+export default HomePage;
