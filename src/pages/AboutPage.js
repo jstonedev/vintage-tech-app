24eec3d15811f3cde4cb1d5a4aa86b15508aac6c
jstonedev <jstonedev521@gmail.com>
2020-10-09 19:53:02 -0400

    modify: add content & styling to about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,9 +2,25 @@ import React from "react";
 
 const AboutPage = () => {
 	return (
-		<div>
-			<h1>About Page</h1>
-		</div>
+		<section className="section about-section">
+			<h1>About Us</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac
+				diam sit amet quam vehicula elementum sed sit amet dui. Curabitur arcu
+				erat, accumsan id imperdiet et, porttitor at sem. Curabitur arcu erat,
+				accumsan id imperdiet et, porttitor at sem. Quisque velit nisi, pretium
+				ut lacinia in, elementum id enim. Pellentesque in ipsum id orci porta
+				dapibus. Curabitur aliquet quam id dui posuere blandit. Lorem ipsum
+				dolor sit amet, consectetur adipiscing elit. Sed porttitor lectus nibh.
+				Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+				Curabitur aliquet quam id dui posuere blandit. Vestibulum ante ipsum
+				primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec
+				velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
+				Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent
+				sapien massa, convallis a pellentesque nec, egestas non nisi.
+				Pellentesque in ipsum id orci porta dapibus.{" "}
+			</p>
+		</section>
 	);
 };
 
